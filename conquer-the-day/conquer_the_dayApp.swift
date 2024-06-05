77bd724b58a4138174d0f3b29d5b630175fdb0c3
Emmett Schillinger <emmettschillinger@gmail.com>
2024-06-05 17:15:45 -0400

    Initial Commit

---
+++ conquer-the-day/conquer_the_dayApp.swift
@@ -0,0 +1,17 @@
+//
+//  conquer_the_dayApp.swift
+//  conquer-the-day
+//
+//  Created by Emmett Schillinger on 6/5/24.
+//
+
+import SwiftUI
+
+@main
+struct conquer_the_dayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
